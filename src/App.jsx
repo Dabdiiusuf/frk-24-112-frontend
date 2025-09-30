50444e21761ba987620e383636f5eb3beceecb05
Elias <elias@st-innovations.net>
2025-09-30 14:32:56 +0200

    added board to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import "./App.css";
-import { GameBoard, Board, ModalWindow } from "frk-24-112-components";
+import { useContext, useCallback } from "react";
+import { ApiContext } from "./providers/ApiContext";
+import { Board } from "frk-24-112-components";
 
-function App() {
-  return <ModalWindow />;
+export default function App() {
+  const { game } = useContext(ApiContext);
+
+  if (!game?.board) return <p>Is Loading</p>;
+
+  const { rows, cols, tiles } = game.board;
+
+  const handleCellClick = useCallback((row, col, value) => {
+    console.log("Clicked:", { row, col, value });
+    //will work with tiles logic here later this is just a start..
+  }, []);
+
+  return (
+    <Board
+      rows={rows}
+      cols={cols}
+      tiles={tiles}
+      size={61}
+      onCellClick={handleCellClick}
+    />
+  );
 }
-
-export default App;
