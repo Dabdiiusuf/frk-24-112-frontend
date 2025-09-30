632b3916f5f1ee04c7ea84c46d81f6f8b174ccdd
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-09-30 12:50:00 +0200

    Fetched the data for Draw game

--- src/providers/ApiContext.jsx
+++ src/providers/ApiContext.jsx
@@ -1,11 +1,12 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, use, useContext, useEffect, useState } from "react";
 import { ConfigContext } from "./ConfigContext";
 
 const ApiContext = createContext(null);
 
-const ApiContextProvider = ({ children, value = {} }) => {
+const ApiContextProvider = ({ children }) => {
   const { API_BASE_URL } = useContext(ConfigContext);
   const [getID, setGetID] = useState("");
+  const [isDraw, setIsDraw] = useState("");
 
   useEffect(() => {
     const fetchGamesID = async () => {
@@ -18,10 +19,26 @@ const ApiContextProvider = ({ children, value = {} }) => {
         console.error(e);
       }
     };
+
+    const fetchDrawGame = async () => {
+      try {
+        const res = await fetch(
+          `${API_BASE_URL}game/88aaf28d-7fef-4028-9e94-7fdbbbd662a0`
+        );
+        const data = await res.json();
+        setIsDraw(data);
+        console.log("Draw game:", data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
     fetchGamesID();
+    fetchDrawGame();
   }, [API_BASE_URL]);
 
-  return <ApiContext.Provider value={value}>{children}</ApiContext.Provider>;
+  return (
+    <ApiContext.Provider value={{ getID }}>{children}</ApiContext.Provider>
+  );
 };
 
 export { ApiContext, ApiContextProvider };
