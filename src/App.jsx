7d43a3c130b7ac91c492b72ba36b942678733bfd
Elias <elias@st-innovations.net>
2025-09-30 15:07:49 +0200

    added GameBoard to app.jsx to show behind Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useContext, useCallback } from "react";
 import { ApiContext } from "./providers/ApiContext";
-import { Board } from "frk-24-112-components";
+import { Board, GameBoard } from "frk-24-112-components";
 
 export default function App() {
   const { isNewGame } = useContext(ApiContext);
@@ -16,12 +16,16 @@ export default function App() {
   const { rows, cols, tiles } = isNewGame.board;
 
   return (
-    <Board
-      rows={rows}
-      cols={cols}
-      tiles={tiles}
-      size={61}
-      onCellClick={handleCellClick}
-    />
+    <div>
+      <GameBoard>
+        <Board
+          rows={rows}
+          cols={cols}
+          tiles={tiles}
+          size={57}
+          onCellClick={handleCellClick}
+        />
+      </GameBoard>
+    </div>
   );
 }
