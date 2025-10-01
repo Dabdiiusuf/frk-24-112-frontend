5f57d07014bec6372321700dac8fbd26f9413929
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-10-01 21:16:58 +0200

    something

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ export default function App() {
             onCellClick={handleCellClick}
           />
         </GameBoard>
-        <GCP />
         {!isNewGame ? (
           <Instructions
             fetchNewGame={fetchNewGame}
